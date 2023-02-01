d4195cb8e0b9b5a55078cb24262a573edc07d339
Sil-Graci <113639238+Sil-Graci@users.noreply.github.com>
2023-02-01 05:27:25 -0300

    antes de nuevas funciones js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,9 @@
 document.write('<style >div.ocultalo{display: none;}</style>');
 
-function MostrarOcultar(texto)
-
-{
-
-    if (document.getElementById)
-
-    {
-
+function MostrarOcultar(texto){
+    if (document.getElementById){
         var aux = document.getElementById(texto).style;
-
         aux.display = aux.display ? "" : "block";
-
         document.getElementById("enlace").text = aux.display ? "Ocultar Perfil" : "Ver Perfil";
-
+        }
     }
-
-}
